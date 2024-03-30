712f960c521eaecc8e2c039f0c1ae75406427272
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-03-29 23:26:56 -0300

    Projects Manager

--- src/components/Dashboard/ProjectsManager.jsx
+++ src/components/Dashboard/ProjectsManager.jsx
@@ -1,9 +1,60 @@
 import styles from './ProjectsManager.module.scss'
 
+import { FaRegEye, FaRegEdit } from "react-icons/fa";
+import { MdDeleteOutline, MdAddCircleOutline } from "react-icons/md";
+import { RiSearch2Line } from "react-icons/ri";
+
 const ProjectsManager = () => {
+  const linhas = Array.from({ length: 10 }, (_, index) => (
+    <tr key={index}>
+      <td>{index + 1}</td>
+      <td>Exemplo {index + 1}</td>
+      <td>Exemplo</td>
+      <td>Exemplo</td>
+      <td><th><FaRegEye /></th></td>
+      <td><th><FaRegEdit /></th></td>
+      <td><th><MdDeleteOutline /></th></td>
+    </tr>
+  ));
+
   return (
     <div className={styles.container}>
-        <div className='skeleton' style={{ width: '100%', height: '100%' }}></div>
+      <div className={styles.header}>
+        <h1>Projetos</h1>
+        <div className={styles.newProject}>Cadastrar<MdAddCircleOutline /></div>
+      </div>
+      <div className={styles.searchContainer}>
+        <input type="text" placeholder='Busque pelo projeto...' />
+        <button type="submit">
+          <RiSearch2Line />
+        </button>
+        <label>
+          <p>Filtrar por: </p>
+          <select name="searchBy">
+            <option value="name">Nome do projeto</option>
+            <option value="id">Id do projeto</option>
+            <option value="createdat">Data de criação</option>
+          </select>
+        </label>
+      </div>
+      <div className={styles.list}>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nome</th>
+              <th>Criado em</th>
+              <th>Atualizado em</th>
+              <th>Visualizar</th>
+              <th>Editar</th>
+              <th>Deletar</th>
+            </tr>
+          </thead>
+          <tbody>
+            {linhas}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
