a38cbfd1262de1ba0224b84a0e6fe2b59a6bc4ed
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-05-02 17:45:20 -0300

    Att

--- src/components/Form/NewFaq.jsx
+++ src/components/Form/NewFaq.jsx
@@ -3,29 +3,97 @@ import styles from './Form.module.scss'
 import { Link } from 'react-router-dom'
 import SystemStatusMessage from './SystemStatusMessage'
 
+// Hooks
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+
+// Redux
+import { resetMessage, createFaq } from '../../slices/faqSlice'
+
 const NewFaq = () => {
+    const [internErrors, setInternErrors] = useState([])
+    const [question, setQuestion] = useState("")
+    const [answer, setAnswer] = useState("")
+
+    const dispatch = useDispatch()
+
+    const { loading: loadingFaq, message: messageFaq, error: errorFaq } = useSelector((state) => state.faq)
+
+    const handleSaveFaq = (e) => {
+        e.preventDefault()
+
+        setInternErrors([])
+
+        if (question.trim() === "") {
+            setInternErrors(prevErrors => [...prevErrors, "A pergunta é obrigatória!"])
+            return
+        }
+
+        if (answer.trim() === "") {
+            setInternErrors(prevErrors => [...prevErrors, "A resposta é obrigatória!"])
+            return
+        }
+
+        const faq = {
+            question: question,
+            answer: answer
+        }
+
+        dispatch(createFaq(faq))
+
+        setQuestion("")
+        setAnswer("")
+        setInternErrors([])
+
+        resetComponentMessage()
+    }
+
+    const resetComponentMessage = () => {
+        setTimeout(() => {
+            dispatch(resetMessage())
+        }, 2000)
+    }
+
     return (
         <div className={styles.mainContainer}>
             <div className={styles.formSections}>
-                <form className={styles.formStep}>
+                <form className={styles.formStep} onSubmit={(e) => handleSaveFaq(e)}>
                     <div className={styles.title}>
                         <h1>Cadastrando uma pergunta/resposta</h1>
                     </div>
                     <label>
                         <p>Qual a pergunta?</p>
-                        <input type="text" placeholder='Digite a pergunta...' />
+                        <input type="text" placeholder='Digite a pergunta...' onChange={(e) => setQuestion(e.target.value)} value={question} />
                     </label>
                     <label>
                         <p>Qual a resposta?</p>
-                        <textarea name="answer" placeholder='Digite a resposta...'></textarea>
+                        <textarea name="answer" placeholder='Digite a resposta...' onChange={(e) => setAnswer(e.target.value)} value={answer}></textarea>
                     </label>
                     <div className={styles.footer}>
-                        <button type="submit" className={styles.submit}>Salvar</button>
-                        <Link to="/adm/painel" className={styles.cancel}>Cancelar</Link>
-                        <div className={styles.messages}>
-                            <SystemStatusMessage type="error" msg="A pergunta é obrigatória!" />
-                            <SystemStatusMessage type="success" msg="Pergunta/resposta cadastrada com sucesso!" />
-                        </div>
+                        {loadingFaq && (
+                            <button type="button" className={styles.submit} disabled>Salvando...</button>
+                        )}
+                        {!loadingFaq && (
+                            <button type="submit" className={styles.submit}>Salvar</button>
+                        )}
+                        <Link to="/adm/painel/faq" className={styles.cancel}>Cancelar</Link>
+                        {internErrors && internErrors.length > 0 && (
+                            <div className={styles.messages}>
+                                {internErrors.map((error) => {
+                                    return <SystemStatusMessage type="error" msg={error} />
+                                })}
+                            </div>
+                        )}
+                        {errorFaq && (
+                            <div className={styles.messages}>
+                                <SystemStatusMessage type="error" msg={errorFaq} />
+                            </div>
+                        )}
+                        {messageFaq && (
+                            <div className={styles.messages}>
+                                <SystemStatusMessage type="success" msg={messageFaq} />
+                            </div>
+                        )}
                     </div>
                 </form>
             </div>
--- src/components/ProjectNewImageForm.jsx
+++ src/components/ProjectNewImageForm.jsx
@@ -1,31 +1,109 @@
 import styles from './ProjectNewImageForm.module.scss'
 
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
+import SystemStatusMessage from './Form/SystemStatusMessage'
 
-import { useState } from 'react'
+// Hooks
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+
+// Redux
+import { resetMessage, createProjectImage } from '../slices/projectImageSlice'
 
 const ProjectNewImageForm = () => {
-    const [urlIsInvalid, setUrlIsInvalid] = useState(false)
-    const [errorMessage, setErrorMessage] = useState("")
+    const { projectId: pId } = useParams()
+
+    const [internErrors, setInternErrors] = useState([])
+    const [image, setImage] = useState(null)
+    const [projectId, setProjectId] = useState(null)
+
+    useEffect(() => {
+        if (pId) {
+            setProjectId(pId)
+        }
+    }, [pId])
+
+    const handleFile = (e) => {
+        const image = e.target.files[0]
+
+        setImage(image)
+    }
+
+    const dispatch = useDispatch()
+
+    const { loading: loadingProjectImage, message: messageProjectImage, error: errorProjectImage } = useSelector((state) => state.projectImages)
+
+    const handleSaveProjectImage = (e) => {
+        e.preventDefault()
+
+        setInternErrors([])
+
+        if (projectId === null) {
+            setInternErrors(prevErrors => [...prevErrors, "O identificador do projeto é obrigatório!"])
+            return
+        }
+
+        const projectImageData = {
+            image: image,
+            projectId: parseInt(projectId)
+        }
+
+        const formData = new FormData()
+
+        Object.keys(projectImageData).forEach((key) => {
+            formData.append(key, projectImageData[key])
+        })
+
+        return console.log(formData)
+        dispatch(createProjectImage(formData))
+
+        setImage("")
+        setInternErrors([])
+
+        resetComponentMessage()
+    }
+
+    const resetComponentMessage = () => {
+        setTimeout(() => {
+            dispatch(resetMessage())
+        }, 2000)
+    }
 
     return (
         <section className={styles.container}>
-            <form>
+            <form onSubmit={(e) => handleSaveProjectImage(e)}>
                 <div className={styles.title}>
                     <h1>Cadastrando Imagem</h1>
                 </div>
                 <label>
                     <p>Selecione a foto que gostaria de adicionar ao projeto</p>
-                    <input type="file" name="image" />
+                    <input type="file" name="image" onChange={(e) => handleFile(e)} />
                 </label>
                 <div className={styles.footer}>
-                    <button type="submit" className={styles.submit}>Enviar</button>
-                    <Link to="/adm/painel/geral" className={styles.cancel}>Cancelar</Link>
-                    <div className={styles.messages}>
-                        {urlIsInvalid === true && (
-                            <SystemStatusMessage type="error" msg={errorMessage} />
-                        )}
-                    </div>
+                    {loadingProjectImage && (
+                        <button type="button" className={styles.submit} disabled>Salvando...</button>
+                    )}
+                    {!loadingProjectImage && (
+                        <button type="submit" className={styles.submit}>Salvar</button>
+                    )}
+                    <Link to="/adm/painel/bancoDeDados" className={styles.cancel}>Cancelar</Link>
+                    {internErrors && internErrors.length > 0 && (
+                        <div className={styles.messages}>
+                            {internErrors.map((error) => {
+                                return <SystemStatusMessage type="error" msg={error} />
+                            })}
+                        </div>
+                    )}
+                    {errorProjectImage && (
+                        <div className={styles.messages}>
+                            <SystemStatusMessage type="error" msg={errorProjectImage} />
+                        </div>
+                    )}
+                    {messageProjectImage && (
+                        <div className={styles.messages}>
+                            <SystemStatusMessage type="success" msg={messageProjectImage} />
+                        </div>
+                    )}
                 </div>
             </form>
         </section>
